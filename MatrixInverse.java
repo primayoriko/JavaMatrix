974d02c08790b2ed2f855326ea070cff6dca390e
primayoriko <48062674+primayoriko@users.noreply.github.com>
2019-09-25 01:16:08 +0700

    Update MInvers

--- MatrixInverse.java
+++ MatrixInverse.java
@@ -3,23 +3,14 @@ import java.lang.Math;
 
 public class MatriksInverse extends Matriks{
     MatriksInverse(int NB, int NK){
-        //KAMUS LOKAL
-		int i,j;
-	    //ALGORITMA
-		this.NB=NB;
-		this.NK=NK;
-		for(i=1;i<=1000;i++){
-			for(j=1;j<=1000;j++){
-				this.el[i][j]=0;
-			}
-		}
+        super(NB,NK);
     }
 
-    public double KofaktorVal(int i, int j){
-        return Math.pow(-1, i+j) * this.el[i][j] * this.Minor(i,j); 
+    public float KofaktorVal(int i, int j){
+        return ((float)Math.pow(-1, i+j)) * this.el[i][j] * this.Minor(i,j); 
     }
 
-    public MatriksInverse KofaktorMatriks(){
+    public MatriksInverse Kofaktor(){
         MatriksInverse kM = new MatriksInverse(this.NB, this.NK);
         for(int i= this.GetFirstIdxBrs() ; i<= this.GetLastIdxBrs() ; i++){
             for(int j= this.GetFirstIdxKol();j<= this.GetLastIdxKol(); j++){
@@ -29,30 +20,24 @@ public class MatriksInverse extends Matriks{
         return kM;
     }
 
-    public MatriksInverse AdjointMatriks(){
-        float temp;
-        MatriksInverse adjM = this.KofaktorMatriks();
-        for(int i= this.GetFirstIdxBrs() ; i<= this.GetLastIdxBrs() ; i++){
-            for(int j= this.GetFirstIdxKol();j<= this.GetLastIdxKol(); j++){
-                temp = adjM.el[i][j];
-                adjM.el[i][j] = adjM.el[j][i];
-                adjM.el[j][i] = temp;
-            }
-        } 
-        return adjM;
+    public MatriksInverse Adjoint(){
+        return this.Kofaktor().Transpose().MatriksToInverse();
     }
 
     public MatriksInverse Inverse(){
-        if(this.Determinan() == 0){
-            System.out.println("Error, determinan tidak boleh nol");
-            return this;
-        }
-        else{
-            return this.AdjointMatriks().KaliKons(1/this.Determinan());
+        return this.Adjoint().KaliKons(1/this.Determinan()).MatriksToInverse();
+    }
+
+    public void Solver(){
+        System.out.println("Solusi :");
+        int i;
+        for(i=this.GetFirstIdxBrs(); i<=this.GetLastIdxBrs(); i++){
+            System.out.printf("X%d = %.3f%n", this.GetLastIdxBrs()-i+1, this.el[i][this.GetFirstIdxKol()]);
         }
     }
 
     public Matriks InverseToMatriks(){
+        int i,j;
 		Matriks Mtemp = new Matriks(this.NB, this.NK);
 		for(i=this.GetFirstIdxBrs();i<=this.GetLastIdxBrs();i++){
 			for(j=this.GetFirstIdxKol();j<=this.GetLastIdxKol();j++){
