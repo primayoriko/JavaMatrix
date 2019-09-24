b439db0b93210e211580dfa80933c7240f3e791e
primayoriko <48062674+primayoriko@users.noreply.github.com>
2019-09-25 01:17:38 +0700

    update Driver

--- DriverMatriks.java
+++ DriverMatriks.java
@@ -1,8 +1,10 @@
 import java.util.Scanner;
 
 public class DriverMatriks{
+    public static Scanner input = new Scanner(System.in);
     public static int inputnum;
     public static boolean loop;
+    public static int[] inputvar = new int[20];
 
     public static void mainMenu(){
         System.out.println("MENU");
@@ -16,6 +18,13 @@ public class DriverMatriks{
         inputMainMenu();
     }
 
+    public static Matriks menuInputMatriks(){
+        System.out.println("Pilih metode Input:");
+        System.out.println("1. Input Langsung Console");
+        System.out.println("2. Input dari File");
+        return inputMatriks();
+    }
+
     public static void menu1(){
         System.out.println("SUBMENU 1");
         System.out.println("1. Metode eliminasi Gauss");
@@ -25,13 +34,33 @@ public class DriverMatriks{
         inputMenu1();
     }
 
+    public static Matriks inputMatriks(){
+        Matriks M = new Matriks(0,0);
+        inputting(1, "pilihan input Matriks");
+        switch (inputvar[1]){
+            case 1 :
+                M.BacaMATRIKS();
+                break;
+            case 2:
+
+
+                break;
+            default:
+                System.out.println("Input salah, coba divalidasi kembali.");
+                //mainMenu();
+                break;
+        }
+        return M;
+    }
+
     public static void inputMainMenu(){
-        inputting();
-        switch (inputnum){
+        inputting(1, "pilihan menu");
+        switch (inputvar[1]){
             case 1 :
                 menu1();
                 break;
             case 2:
+
                 break;
             case 3:
                 break;
@@ -40,40 +69,86 @@ public class DriverMatriks{
             case 5:
                 break;
             case 6:
+                menuInterpolasi();
                 break;
             case 7:
                 loop = false;
                 break;
             default:
                 System.out.println("Input salah, coba divalidasi kembali.");
-                mainMenu();
+                //mainMenu();
                 break;
         }
     }
 
     public static void inputMenu1(){
-        inputting();
-        switch (inputnum){
-            case 1 :
-                
+        inputting(1, "pilihan menu");
+        switch (inputvar[1]){
+            case 1:
+                Gauss();
                 break;
             case 2:
+                GaussJordan();
                 break;
             case 3:
+                MBalikan();
                 break;
             case 4:
                 break;
             default:
                 System.out.println("Input salah, coba divalidasi kembali.");
-                inputMainMenu();
                 break;
         }
     }
 
-    public static void inputting(){
-        Scanner input = new Scanner(System.in);
-        System.out.print("Masukan input : ");
-        inputnum = input.nextInt();
+    public static void Gauss(){
+        Matriks M = menuInputMatriks();
+        if(!M.IsEmpty()){
+            M.MatriksToGauss().Echelon(true).TulisMATRIKS();
+            M.MatriksToGauss().Echelon(true).Solver();
+        }
+    }
+
+    public static void GaussJordan(){
+        Matriks M = menuInputMatriks();
+        if(!M.IsEmpty()){
+            M.MatriksToGauss().Echelon(true).EchelonReduc(true).TulisMATRIKS();
+            M.MatriksToGauss().Echelon(true).EchelonReduc(true).Solver();
+        }
+    }
+
+    public static void MBalikan(){
+        Matriks M = menuInputMatriks();
+        if(!M.IsEmpty()){
+            Matriks B = new Matriks(0,0);
+            B = M.CopyMATRIKS();
+            B.SwapKolom(B.GetFirstIdxKol(), B.GetLastIdxKol());
+            B.ResizeMATRIKS(B.GetLastIdxBrs(), 1);
+            M.ResizeMATRIKS(M.GetLastIdxBrs(), M.GetLastIdxKol()-1);
+            if(M.IsBujurSangkar()){
+                if(M.Determinan()!=0){
+                    M.MatriksToInverse().Inverse().TulisMATRIKS();
+                    B.TulisMATRIKS();
+                    M.MatriksToInverse().Inverse().KaliMATRIKS(B).MatriksToInverse().Solver();
+                }
+                else{
+                    System.out.println("Error, determinan tidak boleh nol");
+                }
+            }
+            else{
+                System.out.println("Error, Matriks harus persegi");
+            } 
+        }
+    }
+
+    public static void menuInterpolasi(){
+        
+    }
+
+    public static void inputting(int i, String A){
+        String s = "Masukan input untuk " + A + " : ";
+        System.out.print(s);
+        inputvar[i] = input.nextInt();
     }
 
     public static void main(String[] args){
@@ -81,7 +156,6 @@ public class DriverMatriks{
         while(loop){
             mainMenu();
         }
-
     }
 
 }
